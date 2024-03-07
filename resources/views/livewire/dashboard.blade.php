6c6b84b4e932f8db8766bcfb5d05bfdb3a882d58
Romer Jasen Jimenez <businessmailromer@gmail.com>
2024-03-08 02:16:04 +0800

    testing navigation

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>Dashbooard</h1>
+
+    <a href="/profile" wire:navigate>Profile</a>
+</div>
---
+++ resources/views/livewire/profile.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>Profile</h1>
+
+    <a href="/dashboard" wire:navigate>Dashboard</a>
+</div>
